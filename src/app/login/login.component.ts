85eb18bf9c1a4eb00ae295e3cfb94cf4dfd3e10c
Manolotsoa <manolotsoadaniel@gmail.com>
2024-04-17 12:05:47 +0300

    Login updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loading: boolean = false;
+  submitted: boolean = false;
+  loginForm: FormGroup;
 
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  getEmailErrorMessage() {
+    console.log(this.loginForm);
+    
+    if (this.loginForm.get('email')?.hasError('required')) {
+      return 'Ce champ est requis!';
+    }
+    return this.loginForm.get('email')?.hasError('email')
+      ? 'Entrez un email valide'
+      : '';
+  }
+
+  getPasswordErrorMessage() {
+    if (this.loginForm.get('password')?.hasError('required')) {
+      return 'Ce champ est requis';
+    }
+
+    return this.loginForm.get('password')?.hasError('password')
+      ? 'Mot de passe incorrecte'
+      : '';
+  }
+
+
+  onSubmit() { }
 }
