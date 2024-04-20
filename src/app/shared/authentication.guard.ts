ea6ae3db803bbf196cae410b18bdba20def1aab7
Manolotsoa <manolotsoadaniel@gmail.com>
2024-04-20 23:12:38 +0300

    Authentication guard

--- src/app/shared/authentication.guard.ts
+++ src/app/shared/authentication.guard.ts
@@ -4,27 +4,13 @@ import { UserService } from '../services/user/user.service';
 import { AuthService } from '../services/auth/auth.service';
 import { LocalService } from '../services/local/local.service';
 import { jwtDecode } from 'jwt-decode';
+import { JwtService } from '../services/jwt/jwt.service';
+import { of, tap } from 'rxjs';
 
 
 export const authenticationGuard: CanActivateFn = (route, state) => {
   // injection du service d'authentification
   const localService = inject(LocalService);
-  const accessToken = localService.getData("accessToken");
-  console.log("Access Token ", accessToken);
-  const tokenDecoded = jwtDecode(accessToken!);
-  if (tokenDecoded) {
-    return true;
-  }
-
-
-  // try {
-  //   if (accessToken) {
-  //     const resultChecked = jwt.verify(accessToken, tokenKey);
-  //     console.log("resultChecked ", resultChecked);
-  //   }
-  // } catch (error) {
-  //   console.log("error ", error);
-  // }
-
-  return false;
+  const jwtService = inject(JwtService);
+  return jwtService.isValid();
 };
