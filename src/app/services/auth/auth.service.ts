0da6d07a4d4409f1a3a01d08c5138aed4ea2862a
Manolotsoa <manolotsoadaniel@gmail.com>
2024-04-21 09:03:44 +0300

    Add globale duplication

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { ArgsLoginTypes } from '../../shared/types';
+import { ArgsRegisterTypes } from '../../shared/types/register';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,10 @@ export class AuthService {
 
   me(): Observable<any> {
     return this.http.post(`${environment?.apiUrl}/me`, undefined);
+  }
+
+  register(args: ArgsRegisterTypes): Observable<any> {
+    return this.http.post(`${environment?.apiUrl}/users`, args)
   }
 
 }
