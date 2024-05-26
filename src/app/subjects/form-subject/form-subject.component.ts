ca49f453f81c82231382534f5831900aec31985f
Manolotsoa Daniel Razafindrakoto <manolotsoadaniel@gmail.com>
2024-05-26 15:32:23 +0300

    Add subject model

---
+++ src/app/subjects/form-subject/form-subject.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-subject',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-subject.component.html',
+  styleUrl: './form-subject.component.css'
+})
+export class FormSubjectComponent {
+
+}
